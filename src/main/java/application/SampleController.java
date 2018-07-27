ebf03cb181e603734543ca0bc728ec5613a96f83
ugurcanarikan <ugurcan@ua-mac.aag-de.ibmmobiledemo.com>
2018-07-27 16:40:15 +0300

    added .jar file

--- src/main/java/application/SampleController.java
+++ src/main/java/application/SampleController.java
@@ -243,46 +243,5 @@ public class SampleController {
 			System.out.println("Speech to text exception");
 		}
 	}
-	private static void getRecommendations() {
-        try {
-            String url = "https://smartroomx.eu-gb.mybluemix.net/ugur";
-            URL obj = new URL(url);
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-            // optional default is GET
-            con.setRequestMethod("GET");
-            //add request header
-            //con.setRequestProperty("User-Agent", "Mozilla/5.0");
-            int responseCode = con.getResponseCode();
-            System.out.println("\nSending 'GET' request to URL : " + url);
-            System.out.println("Response Code : " + responseCode);
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-            //print in String
-            System.out.println(response.toString());
-            //Read JSON response and print
-            /*JSONObject myResponse = new JSONObject(response.toString());
-            System.out.println("result after Reading JSON Response");
-            System.out.println("statusCode- "+myResponse.getString("statusCode"));
-            System.out.println("statusMessage- "+myResponse.getString("statusMessage"));
-            System.out.println("ipAddress- "+myResponse.getString("ipAddress"));
-            System.out.println("countryCode- "+myResponse.getString("countryCode"));
-            System.out.println("countryName- "+myResponse.getString("countryName"));
-            System.out.println("regionName- "+myResponse.getString("regionName"));
-            System.out.println("cityName- "+myResponse.getString("cityName"));
-            System.out.println("zipCode- "+myResponse.getString("zipCode"));
-            System.out.println("latitude- "+myResponse.getString("latitude"));
-            System.out.println("longitude- "+myResponse.getString("longitude"));
-            System.out.println("timeZone- "+myResponse.getString("timeZone"));*/
-        }
-        catch(Exception e){
-            System.out.println(e);
-	    }
-    }
 
 }
